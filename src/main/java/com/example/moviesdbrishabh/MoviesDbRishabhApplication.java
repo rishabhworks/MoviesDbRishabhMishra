a5ec453488e2c57b20694f8870fc087447965510
Rishabh Mishra <mishrarishabh075@gmail.com>
2025-03-04 21:13:02 -0500

    Tested and Connected with MySQL server

---
+++ src/main/java/com/example/moviesdbrishabh/MoviesDbRishabhApplication.java
@@ -0,0 +1,12 @@
+// Student ID: 991753573, Full Name: Rishabh Mishra
+package com.example.moviesdbrishabh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesDbRishabhApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesDbRishabhApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/moviesdbrishabh/MoviesDbRishabhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moviesdbrishabh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesDbRishabhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
